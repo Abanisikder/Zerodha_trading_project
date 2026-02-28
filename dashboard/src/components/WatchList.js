501e68c5c97ec65131182303f492be19fb144fd3
Abani Sikder <abanisikder312@gmail.com>
2026-02-28 21:12:38 +0600

    Material UI installation and watchlist component structure building

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,7 +1,33 @@
+import {watchlist} from "../components/Data/data"
+import React, {useState} from "react"
+import {Tooltip,Grow} from  "@mui/material"
 export default function WatchList(){
     return(
         <>
-        <h1> i am watch list</h1>
+        <div className="watchlist-container">
+            <div className="search-container">
+                <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="search-eg:infy,bse,nifty fut weekly gold mcx"
+                className="search"
+                />
+                <span className="counts">{ watchlist.length}/50</span>
+               
+            </div>
+            <ul className="list">
+               {
+                 watchlist.map((stack,index)=>{
+                    return(
+                        <p>{stack.name}</p>
+                        
+                    )
+
+                })
+               }
+            </ul>
+        </div>
         </>
     )
 }
