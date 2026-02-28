793c66c85b41dab16ff452bb0cac1962397a00de
Abani Sikder <abanisikder312@gmail.com>
2026-02-28 20:43:08 +0600

    positions component created

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,34 +1,47 @@
 import React from "react";
-
-
+import { positions } from "../components/Data/data";
 
 const Positions = () => {
   return (
     <>
-      
-
-     
-
-         
-
-          
-             
-               <table>
+      <h3 className="title">Positions({positions.length})</h3>
+      <div className="order-table">
+        <table>
           <tr>
             <th>Product</th>
-            <th>Instrument</th>
-            <th>Qty.</th>
-            <th>Avg.</th>
+            <th>Name</th>
+            <th>Avg. cost</th>
+             <th>P&L</th>
             <th>LTP</th>
-            <th>P&L</th>
-            <th>Chg.</th>
-          </tr>
-           </table>
-
             
-          
-       
-      
+           
+            <th>Net chg.</th>
+            <th>Day chg.</th>
+          </tr>
+          {positions.map((stock, index) => {
+            const currValue = stock.price * stock.qty;
+            const isProfit = currValue - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isLoss ? "loss" : "profit";
+            return (
+              <>
+                <tr key={index}>
+                  <td>{stock.product}</td>
+                  <td>{stock.name}</td>
+                  <td>{stock.avg.toFixed(2)}</td>
+                  <td>{stock.price.toFixed(2)}</td>
+                
+                  <td className={profClass}>
+                    {(currValue - stock.avg * stock.qty).toFixed(2)}
+                  </td>
+                  <td className={profClass}>{stock.net}</td>
+                  <td className={dayClass}>{stock.day}</td>
+                </tr>
+              </>
+            );
+          })}
+        </table>
+      </div>
     </>
   );
 };
