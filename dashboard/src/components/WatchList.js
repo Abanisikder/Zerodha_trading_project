5e64f67e5b3d9f49fa8e9ff15d971f17bf6c3aa2
Abani Sikder <abanisikder312@gmail.com>
2026-03-02 00:11:51 +0600

    up and down arrow added using icons-material

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,8 +1,8 @@
 import { watchlist } from "../components/Data/data";
 import React, { useState } from "react";
 import { Tooltip, Grow } from "@mui/material";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'; 
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 export default function WatchList() {
   return (
     <>
@@ -19,7 +19,7 @@ export default function WatchList() {
         </div>
         <ul className="list">
           {watchlist.map((stack, index) => {
-           return( <WatchListItem stack={stack} key={index} />)
+            return <WatchListItem stack={stack} key={index} />;
           })}
         </ul>
       </div>
@@ -34,20 +34,21 @@ const WatchListItem = ({ stack }) => {
   let handleMouseLeave = (e) => {
     setShowWatchListAction(false);
   };
-  return(
+  return (
     <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-    <div className="item">
-      <p className={stack.isDown ? "down" : "up"}>{stack.name}</p>
+      <div className="item">
+        <p className={stack.isDown ? "down" : "up"}>{stack.name}</p>
 
-      <div className="itemInfo">
-        <span className="percent">{stack.percent}</span>
-        {
-            stack.isDown? (<keyboardArrowDown className="down"/>):(<keyboardArrowUp className="down"/>)
-        }
-        <span className="price">{stack.price}</span>
+        <div className="itemInfo">
+          <span className="percent">{stack.percent}</span>
+          {stack.isDown ? (
+            <KeyboardArrowDownIcon className="down" />
+          ) : (
+            <KeyboardArrowUpIcon className="up" />
+          )}
+          <span className="price">{stack.price}</span>
+        </div>
       </div>
-    </div>
-  </li>
-  )
+    </li>
+  );
 };
-
